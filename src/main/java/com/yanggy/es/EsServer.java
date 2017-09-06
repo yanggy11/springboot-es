8b4350baba89ee79c0599bcd1bd5610e3cdc9310
yangguiyun <yangguiyun@mobanker.com>
2017-09-06 10:54:50 +0800

    init: base framework

---
+++ src/main/java/com/yanggy/es/EsServer.java
@@ -0,0 +1,15 @@
+package com.yanggy.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yangguiyun on 2017/9/5.
+ */
+
+@SpringBootApplication
+public class EsServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EsServer.class, args);
+    }
+}
